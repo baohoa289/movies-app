39785afe3a33c25e3e97fd03cbcf6fcad2cc04cb
bao hoa <nguyenbhoa9898@gmail.com>
2022-02-28 09:43:02 +0700

    fix-api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
+    baseUrl: 'https://api.themoviedb.org/3',
     apiKey: 'get from themoviedb.org',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
